bd2988f6172417ef1bf13712a046bd0c1cd034b1
hkajiwara <hkajiwara@gmail.com>
2013-09-09 22:47:17 +0900

    Modified

--- server.rb
+++ server.rb
@@ -1,6 +1,11 @@
 enable :sessions
 set :haml, :escape_html => true
 
+configure do
+	set :selected_component, 'ApexClass'
+	set :records, ''
+end
+
 # Top page
 get "/" do
 	haml session[:client] ? :component_type : :guest_home
@@ -27,10 +32,10 @@ get "/logout" do
 	redirect to("/")
 end
 
-# Search
+# Retrieve
 post "/search" do
-	p params[:component_type]
-	case params[:component_type]
+	settings.selected_component = params[:component_type]
+	case settings.selected_component
 		when 'ApexClass'
 			query = 'SELECT ID, Name, Body FROM ApexClass'
 		when 'ApexTrigger'
@@ -43,46 +48,30 @@ post "/search" do
 	end
 
 	begin
-		@result = session[:client].query(query)
+		settings.records = session[:client].query(query)
 	rescue Databasedotcom::SalesforceError => err
 		session[:client] = nil
-		@result = nil
+		settings.records = nil
 		p err.message
 	end
-
 	haml :search
-
 end
 
-# Reference
-get "/sobject/:type" do
-	@sobject = params[:type]
-	@names_or_ids = session[:client].query("SELECT Name,Id FROM #{@sobject}")
-	haml :sobject
-end
-
-# Create page
-get "/sobject/:type/new" do
-	@sobject = session[:client].materialize(params[:type])
-	@record = @sobject.new
-	haml :new_record
-end
-
-# Creatoe
-post "/sobject/:type/create" do
-	owner = params.delete('Owner')
-	splat = params.delete('splat')
-	captures = params.delete('captures')
-	type = params.delete('type')
-	@sobject = session[:client].materialize(type)
-	params['OwnerId'] = session[:client].user_id
-	new_object = session[:client].create(type, params)
-	redirect to("/sobject/#{type}/#{new_object.Id}")
-end
-
-# Reference page
-get "/sobject/:type/:record_id" do
-	@sobject = session[:client].materialize(params[:type])
-	@record = @sobject.find(params[:record_id])
-	haml :record
+# Search
+post "/result" do
+	reg = Regexp.compile(params[:keyword])
+	if settings.selected_component == 'ApexClass' || settings.selected_component == 'ApexTrigger'
+		settings.records.each do |record|
+			if reg =~ record.Body
+				p record.class
+			end
+		end
+	else
+		settings.records.each do |record|
+			if reg =~ record.Markup
+				p record.Name
+			end
+		end
+	end
+	haml :result
 end
